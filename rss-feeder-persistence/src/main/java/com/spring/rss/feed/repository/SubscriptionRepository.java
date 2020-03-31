57f9ebb02afb587da7ecf24bca231ae683207a65
Jacob_Thomas01 <Jacob_Thomas01@BLRKEC124082L.ad.infosys.com>
2020-03-31 17:03:48 +0530

    DONOTPICK - Add Comment#2

--- rss-feeder-persistence/src/main/java/com/spring/rss/feed/repository/SubscriptionRepository.java
+++ rss-feeder-persistence/src/main/java/com/spring/rss/feed/repository/SubscriptionRepository.java
@@ -6,6 +6,7 @@ import com.spring.rss.feed.entity.Subscription;
 
 public interface SubscriptionRepository extends CrudRepository<Subscription, String> {
     //THIS COMMENT SHOULD SHOW IN MASTER AND DEVELOP
+	//THIS COMMENT SHOULD SHOW ONLY IN DEVELOP
 	Subscription findByRssUrl(String rssUrl);
 
 }
