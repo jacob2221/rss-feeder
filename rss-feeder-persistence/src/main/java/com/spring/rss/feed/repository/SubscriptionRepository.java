f11901b0a83a4500ab6bce48fd514b22937c89c4
Jacob_Thomas01 <Jacob_Thomas01@BLRKEC124082L.ad.infosys.com>
2020-03-31 17:02:54 +0530

    PICK - Add Comment#1

--- rss-feeder-persistence/src/main/java/com/spring/rss/feed/repository/SubscriptionRepository.java
+++ rss-feeder-persistence/src/main/java/com/spring/rss/feed/repository/SubscriptionRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 import com.spring.rss.feed.entity.Subscription;
 
 public interface SubscriptionRepository extends CrudRepository<Subscription, String> {
-
+    //THIS COMMENT SHOULD SHOW IN MASTER AND DEVELOP
 	Subscription findByRssUrl(String rssUrl);
 
 }
